2c00e69baab6fad97a20a7ec0da03468676ad281
Artem G <tuocs@yandex.ru>
2017-11-15 02:00:30 +0300

    Changed search result

--- web/commands/searchCommand.php
+++ web/commands/searchCommand.php
@@ -24,12 +24,25 @@ class SearchCommand extends UserCommand
         $pdo = DB::getPdo();
         $sql = $pdo->prepare("SELECT `description`, `photo_id` FROM `posts` WHERE `description` LIKE :keyword");
         $sql->bindValue(':keyword', "%".$text."%");
-        $result = $sql->execute();
+        $sql->execute();
+        $result = $sql->fetchAll();
 
+        $text    = '/search result:' . PHP_EOL;
         $data = [
             'chat_id' => $chat_id,
-            'text'    => implode(PHP_EOL, $result)
+            'text'    => $text,
         ];
-        return Request::sendMessage($data);
+        Request::sendMessage($data);
+
+        foreach( $result as $row ) {
+            $resultData = [
+                'chat_id' => $chat_id,
+                'photo' => $row['photo_id'],
+                'text'    => $row['description']
+            ];
+            Request::sendPhoto($resultData);
+        }
+
+        return true;
     }
 }
